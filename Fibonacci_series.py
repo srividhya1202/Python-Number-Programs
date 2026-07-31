07041d42859ec65d2d27f05b0d55a007fd8dc54c
srividhya1202 <srividhya24td0843@svcet.ac.in>
2026-07-31 03:19:32 -0700

    Update Fibonacci_series.py

--- Fibonacci_series.py
+++ Fibonacci_series.py
@@ -1,8 +1,9 @@
 def fibonacci(n):
-    a = 0
-    b = 1
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+n = int(input("Enter number of terms: "))
+
 for i in range(n):
-        print(a, end=" ")
-        a, b = b, a + b
-n = int(input("Enter the number of terms: "))
-fibonacci(n)
+    print(fibonacci(i), end=" ")
