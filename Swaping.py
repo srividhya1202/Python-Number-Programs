f8db0043cd1547083ef7ff5631a7a987e32f1e07
srividhya1202 <srividhya24td0843@svcet.ac.in>
2026-07-31 03:05:14 -0700

    Update Swaping.py

--- Swaping.py
+++ Swaping.py
@@ -1,7 +1,11 @@
+def swap(a, b):
+    a, b = b, a
+    return a, b
+
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
-a, b = b, a
+a, b = swap(a, b)
 
 print("After swapping:")
 print("a =", a)
